80c0cc45ec4e5023a64c87bd76eef81a798d04bb
meheraz1100 <meheraz1100@gmail.com>
2024-05-17 16:51:53 +0600

    login file added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import img from "../../assets/images/login/login.svg"
 import { useContext } from "react";
 import { Authcontext } from "../../Providers/AuthProviders";
+import axios from "axios";
 
 
 const Login = () => {
 
 
     const {signIn} = useContext(Authcontext);
+    const location = useLocation()
+    const navigate = useNavigate()
+    console.log(location)
 
 
     const handleLogin = e => {
@@ -16,13 +20,20 @@ const Login = () => {
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        
 
 
         signIn(email, password)
         .then(result => {
-            const user = result.user;
-            console.log(user);
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            const user = { email }
+            // navigate(location?.state ? location?.state : '/' ) 
+            // get access token
+            axios.post('http://localhost:5000/jwt', user)
+            .then(res => {
+              console.log(res.data);
+            })
         })
         .catch(error => console.log(error))
     }
@@ -37,7 +48,7 @@ const Login = () => {
             <img src={img} alt="" />
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body"onSubmit={handleLogin}>
+            <form className="card-body" onSubmit={handleLogin}>
             <h1 className="text-4xl text-center font-bold">Login</h1>
               <div className="form-control">
                 <label className="label">
