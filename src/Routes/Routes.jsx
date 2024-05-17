eb0bf8aa2e2a0daceb314126f86bfe4ab5c324d0
meheraz1100 <meheraz1100@gmail.com>
2024-05-17 16:52:42 +0600

    main routes added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,10 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import Checkout from "../Pages/Checkout/Checkout";
+import About from "../Pages/Home/About/About";
+import Bookings from "../Pages/Bookings/Bookings";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
     {
@@ -14,12 +18,25 @@ const router = createBrowserRouter([
             element: <Home></Home>
         },
         {
+          path: '/about',
+          element: <About></About>
+        },
+        {
           path: "/login",
           element: <Login></Login>
         },
         {
           path: "/signUp",
           element: <SignUp></SignUp>
+        },
+        {
+          path: "/checkout/:id",
+          element: <PrivateRoutes><Checkout></Checkout></PrivateRoutes>,
+          loader: ({params}) => fetch(`https://m-59-car-doctor-server.vercel.app/services/${params.id}`)
+        },
+        {
+          path:"/bookings",
+          element: <PrivateRoutes><Bookings></Bookings></PrivateRoutes>
         }
       ]
     },
